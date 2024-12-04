f47824b718a2b26927821407a5ffb031c0b73f36
root <root@Lev.localdomain>
2024-12-04 23:38:27 +0500

    lab5

--- lab5/base5.sh
+++ lab5/base5.sh
@@ -8,7 +8,7 @@ for dir in ~/study/linux_labs/lab*; do
 done
 
 for file in ~/temp_labs/*; do
-    if [ -f "$file" ]; then
+    if [ -f "$file" ]; then #dirname - path to file, basename - only name
         mv "$file" "$(dirname "$file")/Semenov_Lev$(basename "$file")"
     fi
 done
--- lab5/comment5.sh
+++ lab5/comment5.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #! Lab5
 
-for file in ~/temp_labs/*.sh; 
-	do
-    	if [ -f "$file" ]; 
-		then
+for file in ~/temp_labs/*.sh; do
+    	if [ -f "$file" ];
+		then #sed - i без временного файла, 2i - вторая строка
 		sed -i '2i # Author: Semenov Lev' "$file"
     	fi
 done
